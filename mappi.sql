93c268ab4c1226a20582944e446bc3fbe024f15d
Justin Cichra <1342149+jrcichra@users.noreply.github.com>
2020-08-23 10:12:01 -0400

    Add stale device cleaner

--- mappi.sql
+++ mappi.sql
@@ -6,4 +6,35 @@ CREATE TABLE `mappi`.`devices` (
   `last_seen` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `first_seen` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`ip_address`)
-)
+);
+
+--Call this to clear out stale devices
+CREATE PROCEDURE `mappi`.`flush_old_devices`()
+begin
+		DECLARE done INT DEFAULT FALSE;  
+		DECLARE v_last_seen datetime;
+	    DECLARE v_ip_address varchar(255);
+		DECLARE v_hostname varchar(255);
+		declare cursor1 cursor for
+select c.last_seen, c.ip_address, c.hostname
+			from devices a
+			join
+			(select max(last_seen) last_seen, lower(hostname) hostname from devices group by lower(hostname))b
+			on a.last_seen = b.last_seen
+			and a.hostname = b.hostname
+			right join (select last_seen ,ip_address, lower(hostname) hostname from devices) c
+			on c.last_seen < a.last_seen 
+			and c.hostname = b.hostname
+			where a.last_seen is not null
+			order by hostname;
+	DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
+		open cursor1;
+		read_loop: LOOP
+			FETCH cursor1 into  v_last_seen,v_ip_address,v_hostname;
+		    IF done THEN
+      			LEAVE read_loop;
+    		END IF;
+    		delete from devices where ip_address = v_ip_address;
+    	end loop;
+    close cursor1;
+   end
